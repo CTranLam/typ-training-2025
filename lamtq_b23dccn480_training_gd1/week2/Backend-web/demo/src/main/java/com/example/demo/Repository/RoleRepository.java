1a498a8b998c49a989bf41b9e34e5948ff1be0c0
tlam15282@gmail.com <tlam15282@gmail.com>
2025-12-02 08:48:59 +0700

    update mdf

--- lamtq_b23dccn480_training_gd1/week2/Backend-web/demo/src/main/java/com/example/demo/Repository/RoleRepository.java
+++ lamtq_b23dccn480_training_gd1/week2/Backend-web/demo/src/main/java/com/example/demo/Repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.Repository;
 
-public interface RoleRepository {
+import com.example.demo.Entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
+    Optional<RoleEntity> findByRoleCode(String roleCode);
 }
